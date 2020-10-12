a6bb2d57a5d2774f45ba9c47f21aa0d0b7b92ab4
elenaniksy <elfhelen@gmail.com>
2020-10-12 12:06:18 +0300

    feat: add solution for morse decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const exprByWords = [];
+    const exprByAlphabet = [];
+    const quantityLetter = 10;
+    const result = [];
+
+    const sliceStr = (str, number, array) => {
+        let word = [];
+        for (let i = 0; i < str.length; i++) {
+            word.push(str.slice(i, i+number));
+            i += number-1;
+        }
+        array.push(word);
+    }
+
+    expr.split('**********').map(word => sliceStr(word, quantityLetter, exprByWords));
+    exprByWords.map(word => {
+        let wordTemp = [];
+        word.map(letter => {
+            let encodedLetter = '';
+            for(let i = 0; i < letter.length; i++) {
+                if(letter[i] === '1') {
+                    i++;
+                    if(letter[i] === '0') {
+                        encodedLetter += '.';
+                    } else if (letter[i] === '1') {
+                        encodedLetter += '-'
+                    }
+                } else {
+                    encodedLetter += ' ';
+                }
+            }
+            wordTemp.push(encodedLetter.trim());
+        });
+        exprByAlphabet.push(wordTemp);
+    });
+
+    exprByAlphabet.map(encodedWord => {
+        let encWRD = '';
+        encodedWord.map(encodedLetter => encWRD += MORSE_TABLE[encodedLetter]);
+        result.push(encWRD);
+    });
+    return result.join(' ');
 }
 
 module.exports = {
